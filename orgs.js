32c56cb17e3c342fc89a0779c2227f7ac3dc52fa
blueriver212 <53503060+blueriver212@users.noreply.github.com>
2021-06-22 15:44:51 +0100

    Add files via upload

---
+++ orgs.js
@@ -0,0 +1,44 @@
+var orgs = {
+    "Sheet1": [
+        {
+            "Organisation": "GIS-E",
+            "Description": "Our mission is to provide cartographic information that drives positive change. Combining science, aesthetics and innovation, we support organisations specialising in environmental sustainability and social development.",
+            "Job Spec": "Mapping Charity",
+            "Website URL": "https://www.gis-e.org/",
+            "lat": "51.490071",
+            "lng": "-0.182758"
+        },
+        {
+            "Organisation": "The Karen Hilltribes Trust",
+            "Description": "The Karen Hilltribes Trust (KHT) is a community-led organisation that works with Karen communities in Northern Thailand.",
+            "Job Spec": "Providing location maps for their website and funding docs",
+            "Website URL": "https://karenhilltribes.org.uk/",
+            "lat": "18.919329",
+            "lng": "99.845261"
+        },
+        {
+            "Organisation": "East Bali Poverty Project",
+            "Description": "The East Bali Poverty Project (EBPP) is a non-profit organisation helping the thousands of people living in abject poverty without water, sanitation, roads, schools, health facilities and electricity in East Bali.",
+            "Job Spec": "Locations Maps to highlight the villages that they work in",
+            "Website URL": "https://www.eastbalipovertyproject.org/",
+            "lat": "-8.42666",
+            "lng": "115.220122"
+        },
+        {
+            "Organisation": "On Call Africa",
+            "Description": "On Call Africa was set up by a group of doctors and international development experts, all with a shared desire to improve access to healthcare and health outcomes within rural communities. In particular looking to support communities with traditionally poor access to medical professionals and amenities – Zambia was selected due its low ratio of doctors to people. Following a consultation with the Ministry of Health, the Southern Province was selected as the starting point.",
+            "Job Spec": "Creating interactive and static maps to provide volunteer doctors more information about the area that they are working in",
+            "Website URL": "https://www.oncallafrica.org/",
+            "lat": "-15.485609",
+            "lng": "28.123041"
+        },
+        {
+            "Organisation": "Project Amazonas",
+            "Description": "We envision a better and lasting future for the Amazon rainforest and its people. We are a Peruvian-USA non-profit, non-sectarian and non-political organization dedicated to humanitarian, conservation, education and research activities in the Peruvian Amazon region of South America. We are dedicated to supporting the livelihoods and the futures of indigenous and other peoples living in remote areas of the Peruvian Amazon.",
+            "Job Spec": "Geospatial analysis and static maps in order to help them better understand the deforestation problems through earth observation",
+            "Website URL": "https://www.projectamazonas.org/",
+            "lat": "-10.253491",
+            "lng": "-75.771218"
+        }
+    ]
+}
